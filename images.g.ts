c9e7693cb2be782284cf50460bf55165d52d94b1
NixonBoucher <156244710+NixonBoucher@users.noreply.github.com>
2024-02-21 14:49:38 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,158 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . b b b b b b . . . . . 
+. . . b b 2 2 2 2 2 2 b b . . . 
+. . b b 2 2 2 2 2 2 2 2 b b . . 
+. b b 2 d 2 2 2 2 2 2 2 2 b b . 
+. b 2 d 2 2 2 2 2 1 1 1 2 2 b . 
+b 2 d d 2 2 2 2 2 1 1 1 2 2 2 b 
+b 2 d 2 2 2 2 2 2 1 1 1 2 2 2 b 
+b 2 3 2 2 2 2 2 2 2 2 2 1 2 2 b 
+b 5 3 d 2 2 2 2 2 2 2 2 2 2 2 b 
+b 5 3 3 2 2 2 2 2 2 2 2 2 d 2 b 
+b 5 d 3 3 2 2 2 2 2 2 2 d d 2 b 
+. b 5 3 3 3 d 2 2 2 2 d d 5 b . 
+. b d 5 3 3 3 3 3 3 3 d 5 b b . 
+. . b d 5 d 3 3 3 3 5 5 b b . . 
+. . . b b 5 5 5 5 5 5 b b . . . 
+. . . . . b b b b b b . . . . . 
+`;
+            case "image2":
+            case "myImage0":return img`
+. . . . . b b b b b b . . . . . 
+. . . b b 7 7 7 7 7 7 b b . . . 
+. . b b 7 7 7 7 7 7 7 7 b b . . 
+. b b 7 d 7 7 7 7 7 7 7 7 b b . 
+. b 7 d 7 7 7 7 7 1 1 1 7 7 b . 
+b 7 d d 7 7 7 7 7 1 1 1 7 7 7 b 
+b 7 d 7 7 7 7 7 7 1 1 1 7 7 7 b 
+b 7 3 7 7 7 7 7 7 7 7 7 1 7 7 b 
+b 5 3 d 7 7 7 7 7 7 7 7 7 7 7 b 
+b 5 3 3 7 7 7 7 7 7 7 7 7 d 7 b 
+b 5 d 3 3 7 7 7 7 7 7 7 d d 7 b 
+. b 5 3 3 3 d 7 7 7 7 d d 5 b . 
+. b d 5 3 3 3 3 3 3 3 d 5 b b . 
+. . b d 5 d 3 3 3 3 5 5 b b . . 
+. . . b b 5 5 5 5 5 5 b b . . . 
+. . . . . b b b b b b . . . . . 
+`;
+            case "image3":
+            case "myImage1":return img`
+. . . . . b b b b b b . . . . . 
+. . . b b a a a a a a b b . . . 
+. . b b a a a a a a a a b b . . 
+. b b a d a a a a a a a a b b . 
+. b a d a a a a a 1 1 1 a a b . 
+b a d d a a a a a 1 1 1 a a a b 
+b a d a a a a a a 1 1 1 a a a b 
+b a 3 a a a a a a a a a 1 a a b 
+b 5 3 d a a a a a a a a a a a b 
+b 5 3 3 a a a a a a a a a d a b 
+b 5 d 3 3 a a a a a a a d d a b 
+. b 5 3 3 3 d a a a a d d 5 b . 
+. b d 5 3 3 3 3 3 3 3 d 5 b b . 
+. . b d 5 d 3 3 3 3 5 5 b b . . 
+. . . b b 5 5 5 5 5 5 b b . . . 
+. . . . . b b b b b b . . . . . 
+`;
+            case "image4":
+            case "myImage2":return img`
+. . . . . b b b b b b . . . . . 
+. . . b b 9 9 9 9 9 9 b b . . . 
+. . b b 9 9 9 9 9 9 9 9 b b . . 
+. b b 9 d 9 9 9 9 9 9 9 9 b b . 
+. b 9 d 9 9 9 9 9 1 1 1 9 9 b . 
+b 9 d d 9 9 9 9 9 1 1 1 9 9 9 b 
+b 9 d 9 9 9 9 9 9 1 1 1 9 9 9 b 
+b 9 3 9 9 9 9 9 9 9 9 9 1 9 9 b 
+b 5 3 d 9 9 9 9 9 9 9 9 9 9 9 b 
+b 5 3 3 9 9 9 9 9 9 9 9 9 d 9 b 
+b 5 d 3 3 9 9 9 9 9 9 9 d d 9 b 
+. b 5 3 3 3 d 9 9 9 9 d d 5 b . 
+. b d 5 3 3 3 3 3 3 3 d 5 b b . 
+. . b d 5 d 3 3 3 3 5 5 b b . . 
+. . . b b 5 5 5 5 5 5 b b . . . 
+. . . . . b b b b b b . . . . . 
+`;
+            case "image7":
+            case "myImage5":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . 2 2 . . . . . . . 
+. . . . . . 3 1 1 3 . . . . . . 
+. . . . . 2 1 1 1 1 2 . . . . . 
+. . . . . 2 1 1 1 1 2 . . . . . 
+. . . . . . 3 1 1 3 . . . . . . 
+. . . . . . . 2 2 . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image5":
+            case "myImage3":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . 2 2 . . . . . . . 
+. . . . . . 3 a a 3 . . . . . . 
+. . . . . 2 a a a a 2 . . . . . 
+. . . . . 2 a a a a 2 . . . . . 
+. . . . . . 3 a a 3 . . . . . . 
+. . . . . . . 2 2 . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image6":
+            case "myImage4":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . 2 2 . . . . . . . 
+. . . . . . 3 9 9 3 . . . . . . 
+. . . . . 2 9 9 9 9 2 . . . . . 
+. . . . . 2 9 9 9 9 2 . . . . . 
+. . . . . . 3 9 9 3 . . . . . . 
+. . . . . . . 2 2 . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,1 +1,27 @@
-
+namespace SpriteKind {
+    export const Note = SpriteKind.create()
+}
+function spawnNote () {
+    noteList = [assets.image`myImage3`, assets.image`myImage4`, assets.image`myImage5`]
+    note = sprites.create(noteList[randint(0, 2)], SpriteKind.Note)
+}
+function levelSelect (level: number, diff: number) {
+    spawnNote()
+}
+let note: Sprite = null
+let noteList: Image[] = []
+let levelList = [[215], [0], [100]]
+game.splash("Rhythm Madness")
+tiles.setCurrentTilemap(tilemap`level1`)
+let speed = 50
+let mySprite = sprites.create(img`
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    `, SpriteKind.Player)
+mySprite.setStayInScreen(true)
+controller.moveSprite(mySprite, 200, 0)
+mySprite.y = 105
+info.setScore(0)
+levelSelect(game.askForNumber("Songs 1-3", 1), game.askForNumber("Difficulty?"))
